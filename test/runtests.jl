ec20c908fd48a1eb898ef56d657c30825c0a3b74
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2022-03-29 20:20:20 +0530

    Update runtests.jl
    The rationale behind the new tests is that,
    The first test checks if Zygote+Yao is working properly or not. 
    The next one checks if the results are somewhat accurate (from the parameter shift rule and direct Finite Differences)
    If those check out, then the loss is tested, which should be below 1. 
    I believe that this should work out really well for now (and act as somewhat of a base for future methods added)

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,25 @@ using Test
 using Yao: dispatch, EasyBuild, put, Z, chain, Ry, parameters, zero_state, expect
 using Zygote: gradient
 
-@testset "Match trained equation" begin
+≃(a, b) = round(a, digits = 2) ==  round(b, digits = 2)
+
+@testset "Check if chainrules_patch works" begin
+	N = 6	
+	cost = sum([put(N, i=>Z) for i in 1:N])
+	theta = rand(96)
+	new_var = dispatch(EasyBuild.variational_circuit(N,5), :random);
+	function new_loss(theta, j)
+		new_circ = dispatch(new_var, theta)
+		diff_evalue = expect(cost, zero_state(N) => new_circ)
+		return real.(diff_evalue)*j
+	end
+	
+	p = gradient(_theta->new_loss(_theta, 1), theta)[1][3]
+	d = gradient(_theta->new_loss(_theta, 3), theta)[1][3]
+	@test d ≃ p*3
+end
+
+@testset "Tests for Chebyshev sparse mapping" begin
 	M = range(0, stop=0.9, length=20)
         cost = sum([put(6, i=>Z) for i in 1:6])
         var = dispatch(EasyBuild.variational_circuit(6,5), :random)
@@ -12,15 +30,18 @@ using Zygote: gradient
         u_0 = 1.0
         N = 6
         train!(quantum_feature_map_circuit, cost, var, M, N, 1.0, params)
-        
-         e = real.([expect(cost, zero_state(N)=>QuantumNLDiffEq.new_circuit(quantum_feature_map_circuit, var, i, params, N; mapping=QuantumNLDiffEq.Chebyshev())) .+ u_0 .- expect(cost, zero_state(N)=>QuantumNLDiffEq.new_circuit(quantum_feature_map_circuit, var, M[1], params, N; mapping=QuantumNLDiffEq.Chebyshev()))  for i in M])
          
-	≃(a, b) = round(a, digits = 2) ==  round(b, digits = 2)
-	@test e[1] ≃ 1.0
-	@test e[2] ≃ 0.83986
-	@test e[6] ≃ -0.236077
-	@test e[10] ≃ -0.657542
-	@test e[14] ≃ 0.134493
-	@test e[20] ≃ 0.305012
-end
+	@testset "Check if FD works" begin
+		t = 6
+		evalue(x) = expect(cost, zero_state(N)=>QuantumNLDiffEq.new_circuit(quantum_feature_map_circuit, var, x, params, N; mapping=QuantumNLDiffEq.Chebyshev()))
 
+		x_fd = M[t]
+		dx = 0.0001
+		x_fd += dx
+		loss_grad_fd = (evalue(x_fd) - evalue(M[t]))/dx
+
+		@test QuantumNLDiffEq.calculate_diff_evalue(quantum_feature_map_circuit, cost, var, N, params, M[t]; mapping=QuantumNLDiffEq.Chebyshev()) ≃ loss_grad_fd
+	end
+	
+	@test loss(quantum_feature_map_circuit, cost, var, M, N, u_0, params; mapping=QuantumNLDiffEq.Chebyshev()) < 1.0
+end
