7381e468ff347438c85a4864f981caf3d3dad40a
Chetan Vardhan <chetan.verdhan8@gmail.com>
2022-07-23 06:34:10 +0530

    range definition for 1.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using Zygote: gradient
 using DifferentialEquations
 using Flux: Adam
 
+M = range(0; stop=0.9, length=20)
 @testset "Tests for damped oscillation equations" begin
 	function f(u, p, t)
 		λ, κ = p
@@ -18,7 +19,6 @@ using Flux: Adam
 	@testset "Boundary Handling Tests" begin
 		DQC = [QuantumNLDiffEq.DQCType(afm = QuantumNLDiffEq.ChebyshevSparse(2), fm = chain(6, [put(i=>Ry(0)) for i in 1:6]), cost = [Add([put(6, i=>Z) for i in 1:6])], var = dispatch(EasyBuild.variational_circuit(6,5), :random), N = 6)]
 		config(boundary) = DQCConfig(abh = boundary, loss = loss_func)
-		M = range(start=0; stop=0.9, length=20)
 		evalue(M, conf) = [QuantumNLDiffEq.calculate_evalue(DQC[1], DQC[1].cost, prob.u0[1], conf.abh, params[1], M[x], M[1]) for x in 1:length(M)]
 		
 		@testset "Test for Pinned Boundary Handling" begin
@@ -46,7 +46,6 @@ using Flux: Adam
 	@testset "Mapping Tests" begin
 		DQ(mapping) = [QuantumNLDiffEq.DQCType(afm = mapping, fm = chain(6, [put(i=>Ry(0)) for i in 1:6]), cost = [Add([put(6, i=>Z) for i in 1:6])], var = dispatch(EasyBuild.variational_circuit(6,5), :random), N = 6)]
 		config = DQCConfig(abh = QuantumNLDiffEq.Floating(), loss = loss_func)
-		M = range(start=0; stop=0.9, length=20)
 		evalue(M, mapping) = [QuantumNLDiffEq.calculate_evalue(DQC(mapping), DQC(mapping)[1].cost, prob.u0[1], conf.abh, params[1], M[x], M[1]) for x in 1:length(M)]
 		
 		@testset "Test for Product Feature Mapping" begin
@@ -75,7 +74,6 @@ using Flux: Adam
 		@testset "Singlular Encoding" begin
 			DQC = QuantumNLDiffEq.DQCType(afm = QuantumNLDiffEq.ChebyshevSparse(2), fm = chain(6, [put(i=>Ry(0)) for i in 1:6]), cost = [[Add([put(6, i=>Z) for i in 1:6])]], var = dispatch(EasyBuild.variational_circuit(6,5), :random), N = 6)
 			config = DQCConfig(reg = QuantumNLDiffEq.RegularisationParams([[1.0, 0.04724630684751344, -0.7340180576454999, -0.10424985483835963, 0.5322086377394599]], [0.0, 0.18947368421052632, 0.37894736842105264, 0.5684210526315789, 0.7578947368421053], 0.0),  abh = QuantumNLDiffEq.Floating(), loss = loss_func)
-			M = range(start=0; stop=0.9, length=20)
 			evalue(M) = [QuantumNLDiffEq.calculate_evalue(QuantumNLDiffEqChebyshevSparse(2), DQC(mapping)[1].cost, prob.u0[1], config.abh, params[1], M[x], M[1]) for x in 1:length(M)]
 			params = parameters(DQC.var)
 			QuantumNLDiffEq.tr_custom!(DQC, prob, config, M, params)
@@ -86,7 +84,6 @@ using Flux: Adam
 		@testset "Multiple Encoding" begin
 			DQC = [QuantumNLDiffEq.DQCType(afm = QuantumNLDiffEq.ChebyshevSparse(2), fm = chain(6, [put(i=>Ry(0)) for i in 1:6]), cost = [Add([put(6, i=>Z) for i in 1:6])], var = dispatch(EasyBuild.variational_circuit(6,5), :random), N = 6)]
 			config = DQCConfig(reg = QuantumNLDiffEq.RegularisationParams([[1.0, 0.04724630684751344, -0.7340180576454999, -0.10424985483835963, 0.5322086377394599]], [0.0, 0.18947368421052632, 0.37894736842105264, 0.5684210526315789, 0.7578947368421053], 0.0), abh = QuantumNLDiffEq.Floating(), loss = loss_func)
-			M = range(start=0; stop=0.9, length=20)
 			evalue(M) = [QuantumNLDiffEq.calculate_evalue(QuantumNLDiffEqChebyshevSparse(2), DQC(mapping)[1].cost, prob.u0[1], config.abh, params[1], M[x], M[1]) for x in 1:length(M)]
 			params = [parameters(DQC[1].var)]
 			QuantumNLDiffEq.tr_custom!(DQC, prob, config, M, params)
@@ -108,7 +105,6 @@ end
 	config = DQCConfig(abh = QuantumNLDiffEq.Floating(), loss = loss_func)
 	DQC = repeat([QuantumNLDiffEq.DQCType(afm = QuantumNLDiffEq.ChebyshevTower(2), fm = chain(6, [put(i=>Ry(0)) for i in 1:6]), cost = [sum([put(6, i=>Z) for i in 1:6])], var = dispatch(EasyBuild.variational_circuit(6,5), :random), N = 6)], 2)
 	params = [parameters(DQC[1].var), parameters(DQC[2].var)]
-	M = range(start=0; stop=0.9, length=20)
 	QuantumNLDiffEq.train!(DQC, prob, config, M, params; optimizer=Adam(0.02), steps = 400)
 	@test QuantumNLDiffEq.loss(DQC, prob, config, M, params) < 0.05
 end
